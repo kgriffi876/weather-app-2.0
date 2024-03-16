5022f75a4b3eb3fceb4824ba971fce017df2ffec
K08 <162245014+kgriffi08@users.noreply.github.com>
2024-03-15 21:34:45 -0400

    Simplified information on page

--- src/index.js
+++ src/index.js
@@ -10,14 +10,13 @@ function displayTemperature(response) {
 function searchCity(city) {
   let apiKey = "tb40f7o735bc1dcb0b4c9507ada531a4";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiURL);
+
   axios.get(apiKey).then(displayTemperature);
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-city");
-
   searchCity(searchInputElement.value);
 }
 
