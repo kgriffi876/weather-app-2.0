693db7b95b4cc9a1ab3902d859bd71aebbff048c
K08 <162245014+kgriffi08@users.noreply.github.com>
2024-03-26 20:01:44 -0400

    Added forecast HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -56,7 +56,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">⛅</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15&deg;</strong>
+      </div>
+      <div class="weather-forecast-temperature">9&deg;</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-button");
 searchFormElement.addEventListener("click", handleSearchSubmit);
 
 searchCity("Vancouver");
+displayForecast();
